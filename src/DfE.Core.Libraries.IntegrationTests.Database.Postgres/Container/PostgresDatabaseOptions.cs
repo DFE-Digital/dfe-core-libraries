c4b64f777e20092dff499203d49b99aa48249918
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-07-12 17:35:39 +0100

    bring in integration test abstractions, services, database abstractions, postgres testcontainers impl

---
+++ src/DfE.Core.Libraries.IntegrationTests.Database.Postgres/Container/PostgresDatabaseOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DfE.Core.Libraries.IntegrationTests.Database.Postgres.Container;
+
+public sealed class PostgresDatabaseOptions
+{
+#nullable disable
+    public string Database { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+#nullable enable
+}
