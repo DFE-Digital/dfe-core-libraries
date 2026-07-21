f05770f5c7b96fbb736dbc90fc73ea71d93da866
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-07-21 16:24:24 +0100

    fix exposed ports

--- src/DfE.Core.Libraries.IntegrationTests.Database.Postgres/Container/PostgresBuilderContainerFactory.cs
+++ src/DfE.Core.Libraries.IntegrationTests.Database.Postgres/Container/PostgresBuilderContainerFactory.cs
@@ -40,9 +40,7 @@ internal sealed class PostgresBuilderContainerFactory : IContainerFactory
                 .WithDatabase(_dbOptions.Database)
                 .WithUsername(_dbOptions.Username)
                 .WithPassword(_dbOptions.Password)
-                .WithExposedPorts<PostgreSqlBuilder, PostgreSqlContainer, PostgreSqlConfiguration>(
-                    containerPort: PostgreSqlBuilder.PostgreSqlPort,
-                    exposedPort: _containerOptions.PortMappings)
+                .WithExposedPorts<PostgreSqlBuilder, PostgreSqlContainer, PostgreSqlConfiguration>(portMappings)
                 .WithStartupCommands<PostgreSqlBuilder, PostgreSqlContainer, PostgreSqlConfiguration>(_containerOptions.StartupArguments)
                 // Add files that need to be copied into the container before it starts e.g. .sql files to be applied at startup
                 .WithMountedResources<PostgreSqlBuilder, PostgreSqlContainer, PostgreSqlConfiguration>(_containerOptions.CopyResourcesIntoContainerBeforeInit)
