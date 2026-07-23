d23cc5507d05ec98090e6b18fbbce393f5bb6547
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-07-23 10:11:09 +0100

    add classic cor pattern

---
+++ src/DfE.Core.Libraries.DesignPatterns/ChainOfResponsibility/BaseEvaluationHandler.cs
@@ -0,0 +1,35 @@
+﻿namespace DfE.Core.Libraries.DesignPatterns.ChainOfResponsibility;
+
+
+public abstract class BaseEvaluationHandler<TRequest>
+{
+    private BaseEvaluationHandler<TRequest>? _next;
+
+    public BaseEvaluationHandler<TRequest> ChainNext(BaseEvaluationHandler<TRequest> handler)
+    {
+        _next = handler ?? throw new ArgumentNullException(nameof(handler));
+        return this;
+    }
+
+    public async ValueTask HandleAsync(TRequest request, CancellationToken cancellationToken = default)
+    {
+        if (CanHandle(request))
+        {
+
+            await HandleCoreAsync(request, cancellationToken);
+            return;
+        }
+
+        if (_next is null)
+        {
+            throw new InvalidOperationException("No handler was able to process the request.");
+        }
+
+        await _next.HandleAsync(request, cancellationToken);
+
+    }
+
+    public abstract bool CanHandle(TRequest request);
+
+    protected abstract ValueTask HandleCoreAsync(TRequest request, CancellationToken cancellationToken = default);
+}
